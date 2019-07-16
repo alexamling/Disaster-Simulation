af1c3059cae6a646c78027fc5204bdeba7a5b224
Max Kaiser <MaxRKaiser18@gmail.com>
2019-07-16 18:22:07 -0400

    Named the units in the UI, added skeleton script (to main camera) for managing sending units

---
+++ Map Shader Testing/Assets/Disaster Simulation/ObjectiveLoading/ManageUnits.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageUnits : MonoBehaviour
+{
+    public GameObject unitsCount1;
+    public GameObject unitsCount2;
+    public GameObject unitsCount3;
+    public GameObject unitsCount4;
+    public GameObject unitsCount5;
+
+    public objectiveReader objReader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void sendTeam()
+    {
+        //find the currrent objective
+        //use the unitsCounts to update the int value of the playerObjective class associated with that objective
+    }
+
+}
